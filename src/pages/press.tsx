f6a69f01019a2acb9957c9634faf73509e15154b
Juliet Nwagwu Ume-Ezeoke <julietnwagwuume-ezeoke@DN51skp8.SUNet>
2022-03-23 01:30:53 -0700

    videos

--- src/pages/press.tsx
+++ src/pages/press.tsx
@@ -1,21 +1,33 @@
 import Layout from 'components/navigation/layout';
+import Video from 'press/video';
 import React from 'react';
-import { Section } from 'styles/sharedStyles';
+import styled from 'styled-components';
 
 interface PressPageProps {
-    // : string;
+  // : string;
 }
 
-const PressPage: React.FC<PressPageProps> = ({  }) => {
-    return (
-        <Layout
-        pageTitle='Press + Videos'
-       >
+const StyledLayout = styled(Layout)`
+  h2 {
+    margin-bottom: 3em;
+  }
+`;
 
-               Stay tuned for more information!
-       </Layout>
-        
-    );
+const PressPage: React.FC<PressPageProps> = ({}) => {
+  return (
+    <StyledLayout pageTitle="Press + Videos">
+      {/* <h2>Check out highlights from our previous conferences!</h2> */}
+      <Video
+        videoSrcURL="//player.vimeo.com/video/356353115"
+        videoTitle="2019 Forum"
+      ></Video>
+
+      <Video
+        videoSrcURL="//player.vimeo.com/video/289034555 "
+        videoTitle="2018 Forum"
+      ></Video>
+    </StyledLayout>
+  );
 };
 
 export default PressPage;
---
+++ src/press/video.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type VideoProps = {
+  videoSrcURL: string;
+  videoTitle: string;
+};
+
+const Wrapper = styled.div`
+  h3 {
+    margin-bottom: 1em;
+    text-align: center;
+  }
+`;
+
+const FrameHolder = styled.div`
+  width: 80%;
+  height: 40vh;
+  margin: auto;
+  margin-bottom: 5em;
+  position: relative;
+`;
+
+const StyledFrame = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+`;
+
+const Video: React.FC<VideoProps> = ({ videoSrcURL, videoTitle }) => {
+  return (
+    <Wrapper>
+      <h3>{videoTitle}</h3>
+      <FrameHolder>
+        <StyledFrame
+          src={videoSrcURL}
+          title={videoTitle}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          //   webkitallowfullscreen="true"
+          //   mozallowfullscreen="true"
+          allowFullScreen
+        />
+      </FrameHolder>
+    </Wrapper>
+  );
+};
+
+export default Video;
