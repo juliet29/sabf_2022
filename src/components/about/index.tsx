a49dc1fdc4c917114098232f841638a87a5535e0
Juliet Nwagwu Ume-Ezeoke <julietnwagwuume-ezeoke@DN51skp8.SUNet>
2022-04-06 06:42:04 -0700

    dj

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import { SectionSeperator } from 'styles/sharedStyles';
 import GenericSection from './genericSection';
-import ParallaxImage from './parallaxImage';
 import { IndexPageImageQueryQuery } from '../../../graphql-types';
 import ScrollingSection from './scrolling/scrolling';
 import Countdown from './countdown';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
+
+const kem = '../../assets/kem.jpeg';
 
 const Section = styled.section`
   /* color: black; */
   /* margin: 0 10%; */
+`;
+
+const ImageHolder = styled.div`
+  width: 80%;
+  max-height: 50vh;
+  margin: auto;
+  display: block;
+  display: flex;
+  justify-content: center;
+
+  img {
+    object-fit: contain;
+    height: 50vh;
+  }
 `;
 
 interface AboutIndexProps {
@@ -23,6 +40,11 @@ const AboutIndex: React.FC<AboutIndexProps> = ({ data }) => {
       <GenericSection sectionNumber={'01'}></GenericSection>
       <SectionSeperator></SectionSeperator>
       <Countdown></Countdown>
+      <Link to="/attend">
+        <ImageHolder>
+          <StaticImage src={kem} alt="" />
+        </ImageHolder>
+      </Link>
 
       {/* <ParallaxImage
         imageFluid={data.imageOne?.childImageSharp?.fluid}
